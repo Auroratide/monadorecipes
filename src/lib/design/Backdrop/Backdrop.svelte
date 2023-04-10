cfc4fa720737819014263941d12d8e799ffc1cc8
Timothy Foster <tf.auroratide@gmail.com>
2023-04-10 11:29:25 -0500

    Backdrop

---
+++ src/lib/design/Backdrop/Backdrop.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import TornaCamp from "./torna-camp.png";
+</script>
+
+<div class="backdrop" aria-hidden="true" style:--url="url({TornaCamp})"></div>
+
+<style>
+	.backdrop {
+		background-image: var(--url);
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+		pointer-events: none;
+	}
+</style>
--- src/lib/design/Page/Page.svelte
+++ src/lib/design/Page/Page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Header from "./Header.svelte"
 	import { Container } from "$lib/design/Container"
+	import { Backdrop } from "$lib/design/Backdrop"
 
 	export let title: string;
 </script>
@@ -9,6 +10,7 @@
 	<title>{title} | Monado Recipes</title>
 </svelte:head>
 
+<Backdrop />
 <Header {title} />
 <main class="{Container()}">
 	<slot></slot>
