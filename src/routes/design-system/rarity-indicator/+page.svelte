9118ac6c0e97fcb509e01d7c18a64fa79b302655
Timothy Foster <tf.auroratide@gmail.com>
2023-04-14 15:27:49 -0500

    Remove design system routes

--- src/routes/design-system/rarity-indicator/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { RarityIndicator } from "$lib/ingredients/RarityIndicator"
-</script>
-
-<ul>
-	<li>1: <RarityIndicator rarity={1} /></li>
-	<li>2: <RarityIndicator rarity={2} /></li>
-	<li>3: <RarityIndicator rarity={3} /></li>
-</ul>
--- src/routes/design-system/titled-panel/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import "$lib/design/Color"
-	import { TitledPanel } from "$lib/design/TitledPanel"
-</script>
-
-<TitledPanel title="Hello!">
-	<p>Stuff</p>
-</TitledPanel>
