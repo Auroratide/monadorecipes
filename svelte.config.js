ff4ab8009ff6af773bf42d83db07507af9187949
Timothy Foster <tf.auroratide@gmail.com>
2023-04-15 08:43:20 -0500

    Using custom domain - no longer need base path I think

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const BASE_PATH = process.env.BASE_PATH || ''
+const BASE_PATH = process.env.BASE_PATH ?? ''
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
