2487b5bde3cacc7024c28495a5ecda58fb07ed79
Timothy Foster <tf.auroratide@gmail.com>
2023-04-19 18:17:48 -0500

    #4 | Icon for the footer link

--- src/lib/design/Page/Footer.svelte
+++ src/lib/design/Page/Footer.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { FooterBar, FooterItem } from "../FooterBar";
 	import GithubIcon from "../icons/GithubIcon.svelte";
+	import BookIcon from "../icons/BookIcon.svelte";
 	import { Url, External } from "$lib/Url";
 </script>
 
 <FooterBar>
-	<FooterItem href="{Url.legalDisclaimers()}" text="Legal Disclaimers" />
+	<FooterItem href="{Url.legalDisclaimers()}" text="Legal Disclaimers">
+		<BookIcon slot="icon" />
+	</FooterItem>
 	<FooterItem href="{External.source}" text="Open source">
 		<GithubIcon slot="icon" />
 	</FooterItem>
