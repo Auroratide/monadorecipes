a951d41e851bcaf0d5418b4aac107fe7762ae51e
Timothy Foster <tf.auroratide@gmail.com>
2023-05-11 07:00:02 -0500

    ## | I really hate .DS_Store files

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,6 +6,7 @@ const importOne = (file: string): Promise<{ default: Recipe }> =>
 
 export const getAllRecipes = (): Promise<Recipe[]> => {
 	return fs.readdir("./src/data/recipes")
+		.then(files => files.filter(file => file !== ".DS_Store"))
 		.then(files => Promise.all(files.map(importOne)))
 		.then(imported => imported.map(recipe => recipe.default))
 }
