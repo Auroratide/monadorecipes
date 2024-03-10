7184dc64ee40fe39c72e31e6c520d866fdc0b4a7
Timothy Foster <tf.auroratide@gmail.com>
2024-03-10 05:54:09 -0500

    (#7) Add width and height to images

--- src/lib/image/ResponsiveImage/ResponsiveImage.svelte
+++ src/lib/image/ResponsiveImage/ResponsiveImage.svelte
@@ -9,12 +9,14 @@
 	{#each Object.entries(image.sources) as [format, images]}
 		<source srcset="{images.map(i => `${i.src} ${i.w}w`).join(", ")}" type="image/{format}" />
 	{/each}
-	<img src="{image.fallback.src}" {alt} />
+	<img src="{image.fallback.src}" {alt} width="{image.fallback.w}" height="{image.fallback.h}" />
 </picture>
 
 <style>
 	picture, img {
 		display: block;
 		max-width: 100%;
+		width: 100%;
+		height: auto;
 	}
 </style>
