4c9295fb439331b793624c7eb441e8d5e6f84eb8
Auroratide <Auroratide@users.noreply.github.com>
2024-10-01 11:58:13 +0000

    deploy: 2495d869411b28f5addee0fa2489724939d46b64

---
+++ _app/immutable/chunks/Button.DYC0j3jq.js
@@ -0,0 +1,1 @@
+import{E as t}from"./Elevated.DIi0Xf0v.js";import{S as e}from"./site-info.B82GWieF.js";import{T as r}from"./TriangleCorners.DCj9NdBQ.js";const s=()=>`button ${t({useFilter:!0})} ${e.centeredLabel()} ${r({type:"both"})}`;export{s as B};
